bc753d554187c884d3dbcd99a3bdcf68d4a11598
EstevamPonte <tecoponte@gmail.com>
2019-02-22 12:37:14 -0300

    primeiro commit

---
+++ sitepessoal/src/Header/Header.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class Header extends Component {
+    render() {
+        return (
+            <div>
+                <nav className="navbar navbar-light bg-light">
+                    <span className="navbar-brand mb-0 h1"><img src="estevam.PNG"/></span>
+                </nav>
+            </div>
+        );
+    }
+}
+
+export default Header;
